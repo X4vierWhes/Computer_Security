afe7fa3d2c3d53c84179a636a4ae51df5c35e0c0
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2025-10-30 15:59:46 -0300

    wip: Quest1 and Quest2

---
+++ SD_PRATICAOFF1/SD_PRATICAOFF1/src/main/java/org/example/minidns/client/ClientCallbackInterface.java
@@ -0,0 +1,8 @@
+package org.example.minidns.client;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ClientCallbackInterface extends Remote {
+    void notifyUpdate(String updateMessage) throws RemoteException;
+}
